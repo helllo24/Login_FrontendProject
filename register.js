ab0f729935596d1fcf78e5d9147d9a34695e2d90
sudhakar <jsudhakar2415@gmail.com>
2026-04-07 19:20:14 +0530

    Frontend deploye

---
+++ register.js
@@ -0,0 +1,42 @@
+document.getElementById("registerform").addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    //get credendtial
+   const username = document.getElementById("username").value;
+   const password = document.getElementById("password").value;
+   const email = document.getElementById("email").value;
+
+   //call api
+
+   fetch("https://login-employeemanagement-3.onrender.com/auth/register" , {
+    method : "POST",
+     headers:{
+        "Content-type" : "application/json"
+     },
+
+     body: JSON.stringify({
+        username:username,
+        password:password,
+        mail : email
+     })
+   })
+
+   .then(response => response.text())
+   .then(data=>{
+
+
+const returnmsg =document.getElementById("msg").innerText=data;
+
+
+
+
+   })
+  .catch(error => {
+console.error(error);
+});
+
+
+});
+
+
